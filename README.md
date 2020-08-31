c627a96bc806bd1761d88680ac08ea834daa5a41
unknown <patrick@browninglogic.com>
2020-08-30 20:01:14 -0400

    Replace request with axios

--- README.md
+++ README.md
@@ -5,17 +5,11 @@ A simple image proxy built with Node.js and Express
 This is an early POC.  I'll write a proper readme once the project is closer to completion.
 
 TODO
-* Configure CI / CD pipeline
-* Strip out the `set-cookie` response header
-* Filter non-image response types
 * Whitelist by referer
-* Investigate more modern alternatives to [request](https://github.com/request/request)
-* Log response errors
-* Don't pass response body for non-200 status codes
-* Handle 302 if necessary
 * Configure Winston
 * Configure Morgan
 * Log to AppInsights
+* Configurable mime type whitelist / blacklist
 * Gitignored local config
 * Configure linting via `standard`
 * Configure Jasmine
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const request = require('request');
+const axios = require('axios');
 
 const port = process.env.PORT || 3000
 
@@ -9,10 +9,24 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 });
 
+const allowedMimeTypes = [ 'image/jpeg', 'image/png', 'image/x-icon', 'image/vnd.microsoft.icon', 'image/svg+xml' ];
+const disallowedMimeTypes = [ 'text/html; charset=UTF-8', ]
+
 app.get('/image', function(req, res) {
-    var url = req.query.url;
-    request
-        .get(url)
-        .on('error', (err) => console.error('error caught', url, err))
-        .pipe(res);
+  const url = req.query.url;
+  axios({ method: 'get', url, responseType: 'arraybuffer' })
+    .then(response => {
+      const fetchedContentType =  response.headers['content-type'];
+      if (allowedMimeTypes.includes(fetchedContentType)) {
+        res.set('content-type', fetchedContentType);
+        res.status(200);
+        res.end(response.data, 'binary');
+      } else {
+        res.status(502).set('content-type', 'text/plain').send(`Invalid content type: ${fetchedContentType}`);
+      }
+    })
+    .catch(error => {
+      const statusCode = error.response && error.response.status;
+      res.status(statusCode || 500).set('content-type', 'text/plain').send(error.message);
+    });
 });
